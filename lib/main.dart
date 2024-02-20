d3403d229463a7fade96b11e909f5a8135127288
Vincent Wang <376787823@qq.com>
2024-02-20 23:03:52 +0800

    首页换一个好看的lottie动图

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:oktoast/oktoast.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:ms_undraw/ms_undraw.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,6 +54,8 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
+    final appSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -62,11 +65,13 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            UnDraw(
-              height: 150,
-              color: Colors.orange,
-              illustration: UnDrawIllustration.mobile_application,
-            ),
+            Lottie.asset('assets/lottie/1708440553399.json',
+                height: appSize.height / 3),
+            // UnDraw(
+            //   height: 1,
+            //   color: Colors.orange,
+            //   illustration: UnDrawIllustration.cloud_files,
+            // ),
             Container(
               padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
               child: TextField(
@@ -183,7 +188,9 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     } else if (state == AppLifecycleState.resumed) {
       debugPrint("应用进入前台 resumed");
       var clipp = await Clipboard.getData(Clipboard.kTextPlain);
-      _urlController.text = clipp!.text!;
+      if (clipp != null) {
+        _urlController.text = clipp.text!;
+      }
     } else if (state == AppLifecycleState.inactive) {
       // 应用进入非活动状态 , 如来了个电话 , 电话应用进入前台
       // 本应用进入该状态
